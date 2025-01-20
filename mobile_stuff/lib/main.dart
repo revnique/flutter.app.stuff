8c2bacedc9e10ff57651924d04f2ffe58980ec3a
Dominique Harris <revnique@gmail.com>
2025-01-20 11:37:31 -0600

    started overlay

--- mobile_stuff/lib/main.dart
+++ mobile_stuff/lib/main.dart
@@ -100,25 +100,20 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-          child: Stack(children: <Widget>[
-        rive.RiveAnimation.asset(
-          'images/bucktrace.found.animation.riv',
-          onInit: _onRiveInit,
-        ),
+          child: Column(children: <Widget>[
+        Expanded(
+            child: Stack(children: <Widget>[
+          Image(
+            image: AssetImage('images/bt_dollar.jpg'),
+            fit: BoxFit.contain,
+          ),
+          rive.RiveAnimation.asset(
+            'images/bucktrace.found.animation.riv',
+            onInit: _onRiveInit,
+            fit: BoxFit.contain,
+          ),
+        ])),
         Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
